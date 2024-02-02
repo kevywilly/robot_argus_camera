d2ec8eabd50dbb03fa0f02ef8cce17d0cead7b43
Kevin Williams <kevywilly@gmail.com>
2024-02-02 12:36:10 -0500

    addr readme

---
+++ README.md
@@ -0,0 +1,47 @@
+## Waveshare Info
+https://www.waveshare.com/wiki/IMX219-160_Camera
+
+## Metadata
+
+File Name                       : test.dng
+Directory                       : .
+File Size                       : 24 MB
+File Modification Date/Time     : 2021:08:17 16:36:18+01:00
+File Access Date/Time           : 2021:08:17 16:36:18+01:00
+File Inode Change Date/Time     : 2021:08:17 16:36:18+01:00
+File Permissions                : rw-r--r--
+File Type                       : DNG
+File Type Extension             : dng
+MIME Type                       : image/x-adobe-dng
+Exif Byte Order                 : Little-endian (Intel, II)
+Make                            : Raspberry Pi
+Camera Model Name               : /base/soc/i2c0mux/i2c@1/imx477@1a
+Orientation                     : Horizontal (normal)
+Software                        : libcamera-still
+Subfile Type                    : Full-resolution Image
+Image Width                     : 4056
+Image Height                    : 3040
+Bits Per Sample                 : 16
+Compression                     : Uncompressed
+Photometric Interpretation      : Color Filter Array
+Samples Per Pixel               : 1
+Planar Configuration            : Chunky
+CFA Repeat Pattern Dim          : 2 2
+CFA Pattern 2                   : 2 1 1 0
+Black Level Repeat Dim          : 2 2
+Black Level                     : 256 256 256 256
+White Level                     : 4095
+DNG Version                     : 1.1.0.0
+DNG Backward Version            : 1.0.0.0
+Unique Camera Model             : /base/soc/i2c0mux/i2c@1/imx477@1a
+Color Matrix 1                  : 0.8545269369 -0.2382823821 -0.09044229197 -0.1890484985 1.063961506 0.1062747385 -0.01334283455 0.1440163847 0.2593136724
+As Shot Neutral                 : 0.4754476844 1 0.413686484
+Calibration Illuminant 1        : D65
+Strip Offsets                   : 0
+Strip Byte Counts               : 0
+Exposure Time                   : 1/20
+ISO                             : 400
+CFA Pattern                     : [Blue, Green][Green, Red]
+Image Size                      : 4056x3040
+Megapixels                      : 12.3
+Shutter Speed                   : 1/20
--- src/robot_argus_camera/src/camera.cpp
+++ src/robot_argus_camera/src/camera.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <memory>
 #include <opencv2/opencv.hpp>
@@ -7,6 +8,197 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include <signal.h>
+
+struct SensorMode {
+  int id;
+  int width;
+  int height;
+  int framerate;
+};
+
+static const SensorMode sensor_modes[6] = {
+  {0,3264,2464,21},
+  {1,3264,1848,28},
+  {2,1928,1080,29},
+  {3,1640,1232,29},
+  {4,1280,720,59},
+  {5,1280,720,120}
+};
+
+static std::string sensor_mode_to_nvargus_string(int sensor_id, int mode_id) {
+  SensorMode mode = sensor_modes[mode_id];
+  std::stringstream ss;
+  ss << "nvarguscamerasrc sensor_id=" << sensor_id << " sensor_mode=" << mode.id << " ";
+  ss << "! video/x-raw(memory:NVMM), width=(int)" << mode.width << ", ";
+  ss << "height=(int)" << mode.height << ", ";
+  ss << "format=(string)NV12, framerate=(fraction)" << mode.framerate << "/1 ";
+  ss << "! nvvidconv ! video/x-raw, format=(string)I420 ! appsink max-buffers=1 drop=true";
+  return ss.str();
+}
+
+// "nvarguscamerasrc sensor_id=0 sensor_mode=0 ! video/x-raw(memory:NVMM), width=(int)3264, height=(int)2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, format=(string)I420 ! appsink max-buffers=1 drop=true",
+
+
+// Camera Matrix
+cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) <<
+  848.721379, 0.000000, 939.509142, 0.000000, 848.967602, 596.153547, 0.000000, 0.000000, 1.000000
+);
+
+cv::Mat distortionCoefficients = (cv::Mat_<double>(1, 5) << 
+  -0.296850, 0.061372, 0.002562, -0.002645, 0.000000
+);
+
+cv::Mat rectificationMatrix = cv::Mat::eye(3, 3, CV_64F); // Assuming no rectification
+
+cv::Mat projectionMatrix = (cv::Mat_<double>(3, 4) <<
+  452.509735, 0.000000, 648.408514, 0.000000,
+  0.000000, 389.708099, 355.531485, 0.000000,
+  0.000000, 0.000000, 1.000000, 0.000000
+);
+
+class CSICameraPublisher : public rclcpp::Node {
+
+public:
+    CSICameraPublisher(): Node("camera") {
+        
+        declare_parameter("stereo", false);
+	      declare_parameter("undistort",false);
+        declare_parameter("sensor_mode",0);
+        get_parameter("stereo", stereo_);
+        get_parameter("undistort", undistort_);
+        get_parameter("sensor_mode", sensor_mode_);
+
+        capture_delay_millis_ = (int)1000/sensor_modes[sensor_mode_].framerate;
+
+        auto topic0 = stereo_ ? "right/image_raw" : "left/image_raw";
+        auto topic1 = "right_image_raw";
+
+        // Create an image publisher
+        
+        camera_0_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(topic0, 10);
+
+        if(stereo_){
+          camera_1_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(topic1, 10);
+        }
+
+        // Initialize the camera
+
+        if (!_initCamera()) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to initialize the camera.");
+            rclcpp::shutdown();
+        }
+
+        // Start capturing and publishing
+        
+        captureAndPublish();
+    }
+
+  
+  void release() {
+    if(camera_0_.isOpened()) {
+      camera_0_.release();
+    }
+    if(stereo_){
+      if(camera_1_.isOpened()) {
+        camera_1_.release();
+      }
+    }
+
+  }
+
+private:
+
+    bool _initCamera() {
+        // Open the camera using OpenCV
+        camera_0_ = cv::VideoCapture(sensor_mode_to_nvargus_string(0,sensor_mode_));
+        if (!camera_0_.isOpened()) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to open the camera.");
+            return false;
+        }
+
+        if(stereo_) {
+          camera_1_ = cv::VideoCapture(sensor_mode_to_nvargus_string(1,sensor_mode_));
+
+          if (!camera_1_.isOpened()) {
+              RCLCPP_ERROR(this->get_logger(), "Failed to open the camera.");
+              return false;
+          }
+        }
+        return true;
+    }
+
+    bool _capture(cv::VideoCapture cap, cv::Mat &mat) {
+      cv::Mat frame;
+      cv::Mat tmp;
+      cap >> frame;
+      if(!frame.empty()) {
+        if(undistort_) {
+          cv::Mat dst;
+          cv::cvtColor(frame, dst, cv::COLOR_YUV2BGR_I420);
+          cv::undistort(dst, mat, cameraMatrix, distortionCoefficients, cv::noArray());
+        } else {
+          cv::cvtColor(frame, mat, cv::COLOR_YUV2BGR_I420);
+        }
+        
+        //cv::resize(tmp,mat, cv::Size(224,244),cv::INTER_AREA);
+
+        return true;
+      }
+      return false;
+    }
+
+    void captureAndPublish() {
+        // Main loop to capture and publish images
+        while (rclcpp::ok()) {
+            cv::Mat img_0;
+            cv::Mat img_1;
+
+            bool got0 = _capture(camera_0_, img_0);
+            bool got1 = stereo_ ? _capture(camera_1_, img_1) : false;
+
+            if(got0) {
+              auto img_msg_0 = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img_0).toImageMsg();
+              camera_0_publisher_->publish(*img_msg_0);
+            }
+            if(got1) {
+              auto img_msg_1 = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img_1).toImageMsg();
+              camera_1_publisher_->publish(*img_msg_1);
+            }
+
+            if(!rclcpp::ok()) {
+              release();
+            }
+            // Sleep for a while to control the publishing rate
+            rclcpp::sleep_for(std::chrono::milliseconds(capture_delay_millis_));
+        }
+        release();
+        
+    }
+
+    bool stereo_;
+    bool undistort_;
+    int sensor_mode_;
+    int capture_delay_millis_;
+
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_0_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_1_publisher_;
+    
+    rclcpp::Clock clock_;
+    cv::VideoCapture camera_0_;
+    cv::VideoCapture camera_1_;
+    
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CSICameraPublisher>();
+    rclcpp::spin(node);
+    node->release();
+    rclcpp::shutdown();
+
+    return 0;
+}
+
 
 /***
 
@@ -71,151 +263,3 @@ projection
 0.000000 0.000000 1.000000 0.000000
  * 
 */
-
-cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) <<
-        785.45377,   0.     , 701.58699,
-           0.     , 784.15153, 356.23089,
-           0.     ,   0.     ,   1.);
-
-cv::Mat distortionCoefficients = (cv::Mat_<double>(1, 5) << -0.338373, 0.119918, -0.000064, -0.000498, 0.000000);
-
-cv::Mat rectificationMatrix = cv::Mat::eye(3, 3, CV_64F); // Assuming no rectification
-
-cv::Mat projectionMatrix = (cv::Mat_<double>(3, 4) <<
-    615.23431,   0.     , 710.9434 ,   0.     ,
-           0.     , 721.55048, 355.02861,   0.     ,
-           0.     ,   0.     ,   1.     ,   0.);
-
-class CSICameraPublisher : public rclcpp::Node {
-public:
-    CSICameraPublisher()
-        : Node("camera") {
-        
-        declare_parameter("stereo", false);
-	declare_parameter("undistort",false);
-        get_parameter("stereo", stereo_);
-        get_parameter("undistort", undistort_);
-
-        auto topic0 = stereo_ ? "right/image_raw" : "left/image_raw";
-        auto topic1 = "right_image_raw";
-
-        // Create an image publisher
-        camera_0_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(topic0, 10);
-
-        if(stereo_){
-          camera_1_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(topic1, 10);
-        }
-
-        // Initialize the camera
-        if (!initCamera()) {
-            RCLCPP_ERROR(this->get_logger(), "Failed to initialize the camera.");
-            rclcpp::shutdown();
-        }
-
-        // Start capturing and publishing
-        captureAndPublish();
-    }
-
-  void release() {
-    if(camera_0_.isOpened()) {
-      camera_0_.release();
-    }
-    if(stereo_){
-      if(camera_1_.isOpened()) {
-        camera_1_.release();
-      }
-    }
-
-  }
-
-private:
-    bool initCamera() {
-        // Open the camera using OpenCV
-        camera_0_ = cv::VideoCapture("nvarguscamerasrc sensor_id=0 sensor_mode=4 ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)I420 ! appsink max-buffers=1 drop=true");
-        /*
-                                      nvarguscamerasrc sensor_id=ID ! video/x-raw(memory:NVMM), width=X, height=Y, format=(string)NV12 ! nvvidconv flip-method=M ! video/x-raw, format=I420, appsink max-buffers=1 drop=true
-        */
-        if (!camera_0_.isOpened()) {
-            RCLCPP_ERROR(this->get_logger(), "Failed to open the camera.");
-            return false;
-        }
-
-        if(stereo_) {
-          camera_1_ = cv::VideoCapture("nvarguscamerasrc sensor_id=1 sensor_mode=4 ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)I420 ! appsink max-buffers=1 drop=true");
-
-          if (!camera_1_.isOpened()) {
-              RCLCPP_ERROR(this->get_logger(), "Failed to open the camera.");
-              return false;
-          }
-        }
-        return true;
-    }
-
-    bool _capture(cv::VideoCapture cap, cv::Mat &mat) {
-      cv::Mat frame;
-      cv::Mat tmp;
-      cap >> frame;
-      if(!frame.empty()) {
-        if(undistort_) {
-          cv::Mat dst;
-          cv::cvtColor(frame, dst, cv::COLOR_YUV2BGR_I420);
-          cv::undistort(dst, mat, cameraMatrix, distortionCoefficients, cv::noArray());
-        } else {
-          cv::cvtColor(frame, mat, cv::COLOR_YUV2BGR_I420);
-        }
-        
-        //cv::resize(tmp,mat, cv::Size(224,244),cv::INTER_AREA);
-
-        return true;
-      }
-      return false;
-    }
-
-    void captureAndPublish() {
-        // Main loop to capture and publish images
-        while (rclcpp::ok()) {
-            cv::Mat img_0;
-            cv::Mat img_1;
-
-            bool got0 = _capture(camera_0_, img_0);
-            bool got1 = stereo_ ? _capture(camera_1_, img_1) : false;
-
-            if(got0) {
-              auto img_msg_0 = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img_0).toImageMsg();
-              camera_0_publisher_->publish(*img_msg_0);
-            }
-            if(got1) {
-              auto img_msg_1 = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img_1).toImageMsg();
-              camera_1_publisher_->publish(*img_msg_1);
-            }
-
-            if(!rclcpp::ok()) {
-              release();
-            }
-            // Sleep for a while to control the publishing rate
-            rclcpp::sleep_for(std::chrono::milliseconds(33));
-        }
-        release();
-        
-    }
-
-    bool stereo_;
-    bool undistort_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_0_publisher_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_1_publisher_;
-    
-    rclcpp::Clock clock_;
-    cv::VideoCapture camera_0_;
-    cv::VideoCapture camera_1_;
-    
-};
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<CSICameraPublisher>();
-    rclcpp::spin(node);
-    node->release();
-    rclcpp::shutdown();
-
-    return 0;
-}
